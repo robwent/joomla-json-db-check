0682efac0305fe22a9d692cfb90fb17f114eff2c
Dennis Radon <DennisRadon@gmail.com>
2016-11-02 12:06:09 +0100

    Correcting return value of function that ensures JSON code is wrapped by curly braces.

--- json-db-check.php
+++ json-db-check.php
@@ -72,7 +72,7 @@ ob_start();
 	{
 		$data = trim($data);
 
-		return ((substr($data, 0, 1) === '{') && (substr($data, -1, 1) === '}')) ? true : false;
+		return ((substr($data, 0, 1) === '{') && (substr($data, -1, 1) === '}')) ? false : true;
 	}
 
 	function is_json()
